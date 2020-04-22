5f443ebb736503bb73f6104307e5690f3f764087
quinnvillapando <60636265+quinnvillapando@users.noreply.github.com>
2020-04-22 17:34:58 +0800

    [Multiple]
    [add]Error Logs
    [adj]Computation for Transfer Price not yet accurate.
    [add]Request Module Posting(UDT)
    [add]Request Module Required Fields Blocking
    [add]Menu Access
    [adj]Process Button
    [adj]Every Transaction Reload
    [adj]Enter on login
    [add]Request Module-Cancelled Status
    [add]Receipt Sub module-Receipt Records

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -137,89 +137,89 @@ sap.ui.define([
 				}
 			});
 		},
-		loadUDandUDF:function(){
-			//create udt
-			//Payement Processing Draft  Header
-			AppUI5.createTable("APP_OPPD", "Payment Processing - Header", "bott_NoObject");
-			//Payement Processing Details
-			AppUI5.createTable("APP_PPD1", "Payment Processing - Details", "bott_NoObject");
-			//Saved Draft OutGoing Payment
-			AppUI5.createTable("APP_ODOP", "Payment File Extraction - Head", "bott_NoObject");
-			//Saved Draft OutGoing Payment
-			AppUI5.createTable("APP_DOP1", "Payment File Extraction - Det", "bott_NoObject");
-		  //ACTIVITY LOG
-			AppUI5.createTable("APP_ACTIVITYLOGS", "Activity Log", "bott_NoObject");
+		// loadUDandUDF:function(){
+		// 	//create udt
+		// 	//Payement Processing Draft  Header
+		// 	AppUI5.createTable("APP_OPPD", "Payment Processing - Header", "bott_NoObject");
+		// 	//Payement Processing Details
+		// 	AppUI5.createTable("APP_PPD1", "Payment Processing - Details", "bott_NoObject");
+		// 	//Saved Draft OutGoing Payment
+		// 	AppUI5.createTable("APP_ODOP", "Payment File Extraction - Head", "bott_NoObject");
+		// 	//Saved Draft OutGoing Payment
+		// 	AppUI5.createTable("APP_DOP1", "Payment File Extraction - Det", "bott_NoObject");
+		//   //ACTIVITY LOG
+		// 	AppUI5.createTable("APP_ACTIVITYLOGS", "Activity Log", "bott_NoObject");
   
-			//create udf
-			//Payement Processing Header
-			AppUI5.createField("App_DocNum", "Document Number", "@APP_OPPD", "db_Alpha", "", 30);
-			AppUI5.createField("App_DateFrom", "Date From", "@APP_OPPD", "db_Alpha", "", 30);
-			AppUI5.createField("App_DateTo", "Date To", "@APP_OPPD", "db_Alpha", "", 30);
-			AppUI5.createField("App_Suppliercode", "Supplier Code", "@APP_OPPD", "db_Alpha", "", 30);
-			AppUI5.createField("App_SupplierName", "Supplier Name", "@APP_OPPD", "db_Alpha", "", 30);
-			AppUI5.createField("App_TaggingDate", "Tagging Date", "@APP_OPPD", "db_Alpha", "", 30);
-			AppUI5.createField("App_Status", "Status", "@APP_OPPD", "db_Alpha", "", 30);
-			AppUI5.createField("App_Remarks", "Remarks", "@APP_OPPD", "db_Alpha", "", 250);
-			AppUI5.createField("App_CreatedBy", "Created By", "@APP_OPPD", "db_Alpha", "", 30);
-			AppUI5.createField("App_CreatedDate", "Created Date", "@APP_OPPD", "db_Alpha", "", 30);
-			AppUI5.createField("App_UpdatedBy", "Updated By", "@APP_OPPD", "db_Alpha", "", 30);
-			AppUI5.createField("App_UpdatedDate", "Updated Date", "@APP_OPPD", "db_Alpha", "", 30);
-			   AppUI5.createField("App_DraftReference", "Draft Reference", "@APP_OPPD", "db_Alpha", "", 30)
-			//Payement Processing Details
-			AppUI5.createField("App_DocNum", "Document Number", "@APP_PPD1", "db_Alpha", "", 30);
-			AppUI5.createField("App_Priority", "Priority", "@APP_PPD1", "db_Alpha", "", 30);
-			AppUI5.createField("App_InvDocNum", "Invoice DocNum", "@APP_PPD1", "db_Alpha", "", 25);
-			AppUI5.createField("App_InvoiceDocType", "Invoice DocType Type", "@APP_PPD1", "db_Alpha", "", 25);
-			AppUI5.createField("App_InvoiceNo", "Invoice Number", "@APP_PPD1", "db_Alpha", "", 20);
-			AppUI5.createField("App_InvoiceDate", "Invoice Date", "@APP_PPD1", "db_Alpha", "", 30);
-			AppUI5.createField("App_CheckDate", "Check Date", "@APP_PPD1", "db_Alpha", "", 30);
-			AppUI5.createField("App_SuppRefNo", "Supplier Reference No", "@APP_PPD1", "db_Alpha", "", 25);
-			AppUI5.createField("App_Remarks", "Remarks", "@APP_PPD1", "db_Alpha", "", 250);
-			AppUI5.createField("App_InvoiceType", "Invoice Type", "@APP_PPD1", "db_Alpha", "", 10);
-			AppUI5.createField("App_Desc", "Description", "@APP_PPD1", "db_Alpha", "", 250);
-			AppUI5.createField("App_InvoiceCur", "Invoice Currency", "@APP_PPD1", "db_Alpha", "", 30);
-			AppUI5.createField("App_InvoiceTotal", "InvoiceTotal", "@APP_PPD1", "db_Float", "st_Sum", 30);
-			AppUI5.createField("App_RemainingBal", "RemainingBal ", "@APP_PPD1", "db_Float", "st_Sum", 30);
-			AppUI5.createField("App_PaymentAmount", "PaymentAmount ", "@APP_PPD1", "db_Float", "st_Sum", 30);
-			AppUI5.createField("App_CRANo", "CRA Number", "@APP_PPD1", "db_Alpha", "", 30);
-			AppUI5.createField("App_LineNumber", "PaymentAmount ", "@APP_PPD1", "db_Numeric", "", 30);
-			AppUI5.createField("App_CreatedBy", "Created By", "@APP_PPD1", "db_Alpha", "", 30);
-			AppUI5.createField("App_CreatedDate", "Created Date", "@APP_PPD1", "db_Alpha", "", 30);
-			AppUI5.createField("App_UpdatedBy", "Updated By", "@APP_PPD1", "db_Alpha", "", 30);
-			AppUI5.createField("App_UpdatedDate", "Updated Date", "@APP_PPD1", "db_Alpha", "", 30);
-			   AppUI5.createField("App_DraftReference", "Draft Reference", "@APP_PPD1", "db_Alpha", "", 30)
-			// Saved Draft OutGoing Payment
-			AppUI5.createField("App_DocEntry", "Document Entry", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_DocNum", "Batch Number", "@APP_ODOP", "db_Alpha", "", 300);
-			AppUI5.createField("App_PNBPrntBrnch", "PNB Printing Branch", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_DistPatchTo", "Dispatch To", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_DispatchCode", "Dispatch Code", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_DispatchName", "Dispatch Name", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_PNBAccountNo", "PNB Account No", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_PNBAccountName", "PNB Account Name", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_Remarks", "Remarks", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_CreatedBy", "Created By", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_CreatedDate", "Created Date", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_UpdatedBy", "Updated By", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_UpdatedDate", "Updated Date", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_Status", "Status", "@APP_ODOP", "db_Alpha", "", 30);
-			AppUI5.createField("App_DraftNo", "Draft Number", "@APP_ODOP", "db_Alpha", "", 30);
-			//tagging if Outgoing payment is created from this integ
-			AppUI5.createField("App_isFromBankInteg", "Status", "OPDF", "db_Alpha", "", 30);
-			//Payement Processing Details
-			AppUI5.createField("App_DocNum", "Batch Number", "@APP_DOP1", "db_Alpha", "", 30);
-			AppUI5.createField("App_DocEntry", "Document Entry", "@APP_DOP1", "db_Alpha", "", 30);
-			AppUI5.createField("App_DraftNo", "Draft Number", "@APP_DOP1", "db_Alpha", "", 30);
-			AppUI5.createField("App_InvDocNum", "Inv. Document Number", "@APP_DOP1", "db_Alpha", "", 30);
-			AppUI5.createField("App_CreatedBy", "Created By", "@APP_DOP1", "db_Alpha", "", 30);
-			AppUI5.createField("App_CreatedDate", "Created Date", "@APP_DOP1", "db_Alpha", "", 30);
-			AppUI5.createField("App_UpdatedBy", "Updated By", "@APP_DOP1", "db_Alpha", "", 30);
-			AppUI5.createField("App_UpdatedDate", "Updated Date", "@APP_DOP1", "db_Alpha", "", 30);
-			//Add App_BatchNum in A/R invoice once tagged batch
-			AppUI5.createField("App_BatchNum", "Batch Number", "OPCH", "db_Alpha", "", 30);
-			//Add App_BatchNum in A/P Downpayment once tagged batch
-			AppUI5.createField("App_BatchNum", "Batch Number", "ODPO", "db_Alpha", "", 30);
-		  }
+		// 	//create udf
+		// 	//Payement Processing Header
+		// 	AppUI5.createField("App_DocNum", "Document Number", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_DateFrom", "Date From", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_DateTo", "Date To", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_Suppliercode", "Supplier Code", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_SupplierName", "Supplier Name", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_TaggingDate", "Tagging Date", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_Status", "Status", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_Remarks", "Remarks", "@APP_OPPD", "db_Alpha", "", 250);
+		// 	AppUI5.createField("App_CreatedBy", "Created By", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_CreatedDate", "Created Date", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_UpdatedBy", "Updated By", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_UpdatedDate", "Updated Date", "@APP_OPPD", "db_Alpha", "", 30);
+		// 	   AppUI5.createField("App_DraftReference", "Draft Reference", "@APP_OPPD", "db_Alpha", "", 30)
+		// 	//Payement Processing Details
+		// 	AppUI5.createField("App_DocNum", "Document Number", "@APP_PPD1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_Priority", "Priority", "@APP_PPD1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_InvDocNum", "Invoice DocNum", "@APP_PPD1", "db_Alpha", "", 25);
+		// 	AppUI5.createField("App_InvoiceDocType", "Invoice DocType Type", "@APP_PPD1", "db_Alpha", "", 25);
+		// 	AppUI5.createField("App_InvoiceNo", "Invoice Number", "@APP_PPD1", "db_Alpha", "", 20);
+		// 	AppUI5.createField("App_InvoiceDate", "Invoice Date", "@APP_PPD1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_CheckDate", "Check Date", "@APP_PPD1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_SuppRefNo", "Supplier Reference No", "@APP_PPD1", "db_Alpha", "", 25);
+		// 	AppUI5.createField("App_Remarks", "Remarks", "@APP_PPD1", "db_Alpha", "", 250);
+		// 	AppUI5.createField("App_InvoiceType", "Invoice Type", "@APP_PPD1", "db_Alpha", "", 10);
+		// 	AppUI5.createField("App_Desc", "Description", "@APP_PPD1", "db_Alpha", "", 250);
+		// 	AppUI5.createField("App_InvoiceCur", "Invoice Currency", "@APP_PPD1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_InvoiceTotal", "InvoiceTotal", "@APP_PPD1", "db_Float", "st_Sum", 30);
+		// 	AppUI5.createField("App_RemainingBal", "RemainingBal ", "@APP_PPD1", "db_Float", "st_Sum", 30);
+		// 	AppUI5.createField("App_PaymentAmount", "PaymentAmount ", "@APP_PPD1", "db_Float", "st_Sum", 30);
+		// 	AppUI5.createField("App_CRANo", "CRA Number", "@APP_PPD1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_LineNumber", "PaymentAmount ", "@APP_PPD1", "db_Numeric", "", 30);
+		// 	AppUI5.createField("App_CreatedBy", "Created By", "@APP_PPD1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_CreatedDate", "Created Date", "@APP_PPD1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_UpdatedBy", "Updated By", "@APP_PPD1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_UpdatedDate", "Updated Date", "@APP_PPD1", "db_Alpha", "", 30);
+		// 	   AppUI5.createField("App_DraftReference", "Draft Reference", "@APP_PPD1", "db_Alpha", "", 30)
+		// 	// Saved Draft OutGoing Payment
+		// 	AppUI5.createField("App_DocEntry", "Document Entry", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_DocNum", "Batch Number", "@APP_ODOP", "db_Alpha", "", 300);
+		// 	AppUI5.createField("App_PNBPrntBrnch", "PNB Printing Branch", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_DistPatchTo", "Dispatch To", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_DispatchCode", "Dispatch Code", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_DispatchName", "Dispatch Name", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_PNBAccountNo", "PNB Account No", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_PNBAccountName", "PNB Account Name", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_Remarks", "Remarks", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_CreatedBy", "Created By", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_CreatedDate", "Created Date", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_UpdatedBy", "Updated By", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_UpdatedDate", "Updated Date", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_Status", "Status", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_DraftNo", "Draft Number", "@APP_ODOP", "db_Alpha", "", 30);
+		// 	//tagging if Outgoing payment is created from this integ
+		// 	AppUI5.createField("App_isFromBankInteg", "Status", "OPDF", "db_Alpha", "", 30);
+		// 	//Payement Processing Details
+		// 	AppUI5.createField("App_DocNum", "Batch Number", "@APP_DOP1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_DocEntry", "Document Entry", "@APP_DOP1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_DraftNo", "Draft Number", "@APP_DOP1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_InvDocNum", "Inv. Document Number", "@APP_DOP1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_CreatedBy", "Created By", "@APP_DOP1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_CreatedDate", "Created Date", "@APP_DOP1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_UpdatedBy", "Updated By", "@APP_DOP1", "db_Alpha", "", 30);
+		// 	AppUI5.createField("App_UpdatedDate", "Updated Date", "@APP_DOP1", "db_Alpha", "", 30);
+		// 	//Add App_BatchNum in A/R invoice once tagged batch
+		// 	AppUI5.createField("App_BatchNum", "Batch Number", "OPCH", "db_Alpha", "", 30);
+		// 	//Add App_BatchNum in A/P Downpayment once tagged batch
+		// 	AppUI5.createField("App_BatchNum", "Batch Number", "ODPO", "db_Alpha", "", 30);
+		//   }
 
 
   });
