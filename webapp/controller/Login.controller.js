223b4b3ff3e04222b63dc6867d19780b5afb03f2
quinnvillapando <60636265+quinnvillapando@users.noreply.github.com>
2022-05-05 15:20:43 +0800

    [updated LOGIN]

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -102,7 +102,7 @@ sap.ui.define([
         fGetAllRecords: function(queryTag){
 			// var aReturnResult = [];
 			$.ajax({
-				url: "https://xs.biotechfarms.net/app_xsjs/ExecQuery.xsjs?dbName=051021_TEST_DEV_PROD_BIOTECH&procName=spAppBusinessUnit&QUERYTAG="+ queryTag +"&value1=&value2=&value3=&value4=",
+				url: "https://xs.biotechfarms.net/app_xsjs/ExecQuery.xsjs?dbName=011122_PROD_BIOTECH&procName=spAppBusinessUnit&QUERYTAG="+ queryTag +"&value1=&value2=&value3=&value4=",
 				type: "GET",
 				dataType: "json",
 				beforeSend: function (xhr) {
